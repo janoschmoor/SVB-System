a4920087731eda05e0ec21280d29a554cdaf05a0
Jonas Lüthy <jonasluethy@iphone-xs-von-jonas.home>
2022-12-31 12:56:18 +0100

    changes 31.12.22 - 12:56

---
+++ githubentry/development/front-end/vite-project/src/App.tsx
@@ -0,0 +1,17 @@
+import { Card } from 'react-bootstrap'
+
+function App() {
+
+  return (
+    <>
+    <Card>
+      <Card.Body>
+        hallo was geht?
+      </Card.Body>
+    </Card>
+  
+    </>
+  )
+}
+
+export default App
